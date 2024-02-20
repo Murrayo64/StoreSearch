7169d93e9ec626729400d49cacb44d93a2f9a193
Joe Murray <jmurray45@gmail.com>
2024-02-20 15:31:44 -0500

    pg 821

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -26,4 +26,16 @@ extension SearchViewController: UISearchBarDelegate {
         print("The search text is: '\(searchBar.text!)'")
     }
 }
+// MARK: - Table View Delegate
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return 0
+        }
+    func tableView(
+        _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            return UITableViewCell()
+        }
+}
 
