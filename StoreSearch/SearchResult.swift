39b95638d6207e1d558f9f74af4865a11753157d
Joe Murray <jmurray45@gmail.com>
2024-02-22 12:13:41 -0500

    pg 887

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -13,13 +13,20 @@ class ResultArray: Codable {
 }
 
 class SearchResult: Codable, CustomStringConvertible {
+    var kind: String? = ""
     var artistName: String? = ""
     var trackName: String? = ""
+    var trackPrice: Double? = 0.0
+    var currency = ""
+    var artworkUrl60 = ""
+    var artworkUrl100 = ""
+    var trackViewUrl: String? = ""
+    var primaryGenreName = ""
     
     var name: String {
         return trackName ?? ""
     }
     var description: String {
-        return "\nResult - Name: \(name), Artist Name: \(artistName ?? "None")"
+        return "\nResult - Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")"
     }
 }
