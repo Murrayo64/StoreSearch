1c87572b0a8ce669d1c3628be50d52783bde4740
Joe Murray <jmurray45@gmail.com>
2024-02-23 14:49:02 -0500

    pg 941

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -25,5 +25,65 @@ class SearchResultCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+    // MARK: - Helper Methods
+    func configure(for result: SearchResult) {
+        nameLabel.text = result.name
+        
+        if result.artist.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", result.artist, result.type)
+        }
+    }
 }
+// MARK: - Table View Delegate
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            if isLoading {
+              return 1
+            } else if !hasSearched {
+              return 0
+            } else if searchResults.count == 0 {
+              return 1
+            } else {
+              return searchResults.count
+            }
+            
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            if isLoading {
+                let cell = tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.loadingCell,
+                                                         for: indexPath)
+                let spinner = cell.viewWithTag(100) as! UIActivityIndicatorView
+                spinner.startAnimating()
+                return cell
+            } else
+            
+            if searchResults.count == 0 {
+                return tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.nothingFoundCell, for: indexPath)
+            } else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
+                
+                let searchResult = searchResults[indexPath.row]
+                cell.configure(for: searchResult)
+                return cell
+            }
+            
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+        
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if searchResults.count == 0 || isLoading {
+            return nil
+        } else {
+            return indexPath
+        }
+    }
+}
+
